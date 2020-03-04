d6ccfa0a49aa5805fad670601f0bb8c2e0317516
Xiaoyi Liu <lxy878@gmail.com>
2020-03-04 22:49:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
+
 def oxford_comma(array)
 
+  if array[-2].nil?
+    return array.join
+  end
+  s = array[0..-2].join(", ")
+  return array[0]==array[-2]? s << " and "+array[-1] : s << ", and "+array[-1]
 end
