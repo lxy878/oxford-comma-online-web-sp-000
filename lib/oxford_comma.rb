a25d438bd6011130137743010d76816dbec1f168
Xiaoyi Liu <lxy878@gmail.com>
2020-03-04 22:35:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ def oxford_comma(array)
   if array[-2].nil?
     return array.join
   end
-  s = array[0..-2].join(" ")
-  s << " and "+array[-1]
+  s = array[0..-2].join(", ")
+  s << "and "+array[-1]
   binding.pry
 end
 
